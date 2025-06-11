44d7a73ff07c8d2b932904ee0fe0e537d702af89
halilibrahimsaltas <h.ibrahimsaltas@gmail.com>
2025-06-12 00:30:35 +0300

    Added new modules: BookModule, WordModule, AudioModule, SearchModule and FileModule.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,14 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { BookModule } from './book/book.module';
+import { WordModule } from './word/word.module';
+import { AudioModule } from './audio/audio.module';
+import { SearchModule } from './search/search.module';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [AuthModule, UserModule, AuthModule],
+  imports: [AuthModule, UserModule, AuthModule, BookModule, WordModule, AudioModule, SearchModule, FileModule],
   controllers: [AppController, UserController, AuthController],
   providers: [AppService, UserService, AuthService],
 })
