5ca7cde73ffbb6aa6cc6a28674fa44c75a56dd2c
halilibrahimsaltas <h.ibrahimsaltas@gmail.com>
2025-06-12 00:26:41 +0300

    User and auth modules were created.

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+
+@Module({
+  imports: [AuthModule, UserModule, AuthModule],
+  controllers: [AppController, UserController, AuthController],
+  providers: [AppService, UserService, AuthService],
+})
+export class AppModule {}
