eb35768796201a1434bf9eb9896b23bf4b5b0f59
halilibrahimsaltas <h.ibrahimsaltas@gmail.com>
2025-07-03 00:29:06 +0300

    new development for mobil

---
+++ frontend/app.config.js
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'lexify-mobile',
+    slug: 'lexify-mobile',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './src/assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './src/assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './src/assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      favicon: './src/assets/favicon.png',
+      bundler: 'metro',
+    },
+    extra: {
+      API_BASE_URL: process.env.API_BASE_URL,
+    },
+  },
+};
