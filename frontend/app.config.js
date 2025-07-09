986c95fe17c41add18cec0fe9ee036007987cfa2
halilibrahimsaltas <h.ibrahimsaltas@gmail.com>
2025-07-09 23:51:50 +0300

    Firebase integration and configuration updates
    - Firebase-related packages added and configured
    - Updates made to the app.config.js and package.json files
    - Changed the app icon
    - Added the Android package name (android.package)
    - Updated the new owner information and extra settings (extra.eas.projectId)
    - Updated the expo-splash-screen dependency

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: 'lexify-mobile',
     version: '1.0.0',
     orientation: 'portrait',
-    icon: './src/assets/icon.png',
+    icon: './src/assets/icon/Lexify_icon.png',
     userInterfaceStyle: 'light',
     newArchEnabled: true,
     splash: {
@@ -23,9 +23,11 @@ export default {
       supportsTablet: true,
     },
     android: {
+      package: "com.halil354.lexify",
       adaptiveIcon: {
         foregroundImage: './src/assets/adaptive-icon.png',
         backgroundColor: '#ffffff',
+
       },
       edgeToEdgeEnabled: true,
     },
@@ -33,12 +35,16 @@ export default {
       favicon: './src/assets/favicon.png',
       bundler: 'metro',
     },
+    owner: "halil354",
     extra: {
       API_BASE_URL: process.env.API_BASE_URL,
       GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
       GOOGLE_IOS_CLIENT_ID: process.env.GOOGLE_IOS_CLIENT_ID,
       GOOGLE_ANDROID_CLIENT_ID: process.env.GOOGLE_ANDROID_CLIENT_ID,
       GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+      eas: {
+        projectId: "8de92b65-e9b2-414d-9b62-23928fe5d658",
+      },
     },
     scheme: 'lexify-mobile',
   },
